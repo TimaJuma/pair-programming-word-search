b569ff98f06ec84b5c588b84e47e48c674845b53
TimaJuma <tima.xpl@gmail.com>
2020-07-30 02:10:49 +0000

    refractored code for word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
+// function accept 2D array of letters an if passed word is found vertically or horizontally
+const wordSearch = (letters, word) => {
+
+    // create 2 array with words vertically and horizontally combining letters
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+    if (horizontalJoin.includes(word) || verticalJoin.includes(word)) {
+        return true;
+    } else {
+        return false
     }
 }
 
-module.exports = wordSearch
+
+// helper function to transpose 2D array
+const transpose = (letters) => {
+    const rows = letters.length;
+    const cols = letters[0].length;
+    const grid = [];
+
+    for (let j = 0; j < cols; j++) {
+        grid[j] = Array(rows);
+    }
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[j][i] = letters[i][j];
+        }
+    }
+    return grid;
+};
+
+module.exports = wordSearch;
